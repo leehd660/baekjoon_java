1327d8a98d2f9dc4938ff611f91d62e827aed008
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-09-30 10:55:29 +0900

    09.30 평균은 넘겠지 문제 해결. 수업시간에 배운 @SuppressWarnings("unchecked")도 쓰고 반올림할 수 있는 String.format("%.3f", value)도 써서 풀었다

---
+++ src/bronze1/평균은넘겠지.java
@@ -0,0 +1,35 @@
+package bronze1;
+
+import java.util.Scanner;
+
+@SuppressWarnings("unchecked")
+public class 평균은넘겠지 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int classNum = scanner.nextInt();
+        String[] classArr = new String[classNum];
+
+        for (int i=0; i<classNum; i++){
+            int stuNum = scanner.nextInt();
+            int totalNum = 0;
+            int[] arr = new int[stuNum];
+            for (int j=0; j<stuNum; j++){
+                int score = scanner.nextInt();
+                arr[j] = score;
+                totalNum += score;
+            }
+            double m = (double) totalNum /stuNum;
+            int count = 0;
+            for (int ans : arr){
+                if (ans>m){
+                    count += 1;
+                }
+            }
+            double percent = ((double)count/stuNum)*100;
+            classArr[i] = String.format("%.3f",percent) + "%";
+        }
+        for (String s : classArr){
+            System.out.println(s);
+        }
+    }
+}
