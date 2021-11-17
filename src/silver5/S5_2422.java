0bd9b7c49e64d9496291e688f70e34b55cbcce35
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-11-17 17:45:56 +0900

    11.17 한윤정이 이탈리아~ 문제 미해결. 시간 초과남.

---
+++ src/silver5/S5_2422.java
@@ -0,0 +1,49 @@
+package silver5;
+
+import java.io.*;
+
+public class S5_2422 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String nmStr = bf.readLine();
+        String[] nmArr = nmStr.split(" ");
+        int N = Integer.parseInt(nmArr[0]);
+        int M = Integer.parseInt(nmArr[1]);
+        int[][] check = new int[M][2];
+        for (int i=0; i<M; i++) {
+            String notaste = bf.readLine();
+            String[] notasteArr = notaste.split(" ");
+            int a = Integer.parseInt(notasteArr[0]);
+            int b = Integer.parseInt(notasteArr[1]);
+            check[i][0] = a;
+            check[i][1] = b;
+        }
+        int answer =0;
+        for (int i=1; i<=N-2; i++) {
+            for (int j=i+1; j<=N-1; j++) {
+                for (int k=j+1; k<=N; k++) {
+                    if (test(check,i,j,k)){
+                        continue;
+                    }
+                    else {
+                        answer ++;
+                    }
+                }
+            }
+        }
+        bw.write(String.valueOf(answer));
+        bw.close();
+    }
+
+    static boolean test(int[][] table, int a,int b,int c){
+        boolean answer = false;
+        for (int i=0; i<table.length; i++) {
+            if ( (table[i][0] == a || table[i][0] == b || table[i][0] == c) && (table[i][1] == a || table[i][1] == b || table[i][1] == c)){
+                answer = true;
+                break;
+            }
+        }
+        return answer;
+    }
+}
