d2ced59b312715c448b957d4f61779e617dd290b
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-11-27 17:17:45 +0900

    11.27 2Xn타일 문제 미해결. 다른 사람들 풀이보니까 재귀함수로 많이 푼거 같은데 조합으로 푸는게 훨씬 간단해보이지만, 그럴 경우 overflow되는 경우를 해결하지 못했음.

--- src/silver3/S3_11726.java
+++ src/silver3/S3_11726.java
@@ -13,18 +13,30 @@ public class S3_11726 {
         for (int i=1; i<=maxTwo; i++){
             int oneNum = num-(i*2);
             int totalNum = oneNum+i;
-            answer += (factorial(totalNum) / (factorial(i)*factorial(oneNum)))%10007;
+
         }
-//        answer = answer%10007;
+        answer = answer%10007;
+
         bw.write(String.valueOf(answer));
         bw.close();
     }
 
-    static long factorial(int n) {
-        long answer = 1;
-        for (int i=1; i<=n; i++) {
-            answer = (answer*i%10007);
+    static int calcul(int num) {
+        int answer = 1;
+        if (num == 1){
+            return answer;
         }
         return answer;
     }
+
+//    static double combination(int num1, int num2) {
+//        double answer = 1;
+////        for (int i=1; i<=n; i++) {
+////            answer = (answer*i%10007);
+////        }
+//        for (int i=0; i< num1; i++){
+//            answer = (answer%10007) * (((num1+num2-i)/(double)(num1-i))%10007);
+//        }
+//        return answer;
+//    }
 }
