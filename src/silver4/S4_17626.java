e2d821cc424620d868943dbfa54ae3fbcbfdafa1
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-12-09 14:49:56 +0900

    12.09 Four squares 문제 해결. DP를 공부했음. DP를 하기 위해서는 일단 문제에서 점화식을 찾아서 식을 세우는 것이 중요하고, 다른 조건들은 생각하여 조건문을 사용하면 된다.

--- src/silver4/S4_17626.java
+++ src/silver4/S4_17626.java
@@ -8,11 +8,47 @@ public class S4_17626 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String numStr = bf.readLine();
         int num = Integer.parseInt(numStr);
-        int[] table = new int[223];
-        for (int i=1; i<=223; i++) {
-            table[i-1] = (int)Math.pow(i,2);
+        int[] numArr = new int[num+1];
+        numArr[0] = 0;
+        numArr[1] = 1;
+        int nowNum = 1;
+        while (nowNum<=num) {
+            if (Math.pow((int)Math.sqrt(nowNum),2) == nowNum) {
+                numArr[nowNum] = 1;
+            }
+            else {
+                int min = Integer.MAX_VALUE;
+                for (int i=1; i<=(int)Math.sqrt(nowNum); i++){
+                    min = Math.min(numArr[(int)Math.pow(i,2)] + numArr[nowNum-(int)Math.pow(i,2)], min);
+                }
+                numArr[nowNum] = min;
+            }
+            nowNum++;
         }
-        int nRoot = (int)Math.sqrt(num);
+        bw.write(String.valueOf(numArr[num]));
+        bw.close();
 
+
+//        int[] table = new int[223];
+//        for (int i=1; i<=223; i++) {
+//            table[i-1] = (int)Math.pow(i,2);
+//        }
+//        int nRoot = (int)Math.sqrt(num);
+
+
+//        int nowNum = 1;
+//        int nowN = 0;
+//        int beforeNum = 1;
+//        int beforeN = 0;
+//        while (nowNum < num) {
+//            int root = (int)Math.sqrt(nowNum);
+//            if (Math.pow(root,2) == nowNum) {
+//                nowN = 1;
+//            }
+//            else {
+//                nowN = beforeN
+//            }
+//            nowNum++;
+//        }
     }
 }
