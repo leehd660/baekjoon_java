5ed74bfaa641502844655a95fbaed60bf76a4620
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-12-25 18:58:06 +0900

    12.25 바이러스 문제 해결. 예전에 틀렸던거 BFS방법으로 풀어서 해결함. 배열 안에 arraylist 넣는거에서 굉장히 헷갈렸는데, 생성자를 만들어주면서 해결했다. 근데 왜 145번째 줄에서 데이터를 int형으로 바꿔줘야하는지 모르겠음..생성자를 Integer로 해줬는데...?

--- src/silver3/S3_2606.java
+++ src/silver3/S3_2606.java
@@ -1,7 +1,10 @@
 package silver3;
 
 import java.io.*;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 public class S3_2606 {
 //    public static void main(String[] args) throws IOException {
@@ -59,39 +62,91 @@ public class S3_2606 {
 //        bw.write(String.valueOf(answer));
 //        bw.close();
 //    }
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        String allComputerNum = bf.readLine();
+//        String numStr = bf.readLine();
+//        int num = Integer.parseInt(numStr);
+//        Map<Integer, List<Integer>> map = new HashMap<>();
+//        for (int i=0; i<num; i++) {
+//            String net = bf.readLine();
+//            String[] netArr = net.split(" ");
+//            int a = Integer.parseInt(netArr[0]);
+//            int b = Integer.parseInt(netArr[1]);
+//            if (map.keySet().contains(a)){
+//                map.get(a).add(b);
+//            }
+//            else {
+//                List<Integer> list = new ArrayList<>();
+//                list.add(b);
+//                map.put(a,list);
+//            }
+//            if (map.keySet().contains(b)){
+//                map.get(b).add(a);
+//            }
+//            else {
+//                List<Integer> list = new ArrayList<>();
+//                list.add(a);
+//                map.put(b,list);
+//            }
+//        }
+//        boolean[] vst = new boolean[Integer.parseInt(allComputerNum)+1];
+//        Queue<Integer> queue = new ArrayDeque<>();
+//        queue.add(1);
+//        vst[1] = true;
+//
+//    }
+
+    // 12월 25일 재도전
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String allComputerNum = bf.readLine();
-        String numStr = bf.readLine();
-        int num = Integer.parseInt(numStr);
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i=0; i<num; i++) {
-            String net = bf.readLine();
-            String[] netArr = net.split(" ");
-            int a = Integer.parseInt(netArr[0]);
-            int b = Integer.parseInt(netArr[1]);
-            if (map.keySet().contains(a)){
-                map.get(a).add(b);
+        int comNum = Integer.parseInt(bf.readLine());
+        int pairCom = Integer.parseInt(bf.readLine());
+//        List<Integer> inner = new ArrayList<>();
+        List[] table = new List[comNum+1];
+        for (int i=0; i<comNum+1; i++) {
+            List<Integer> inner = new ArrayList<>();
+            table[i] = inner;
+        }
+        for (int i=0; i<pairCom; i++) {
+            String pair = bf.readLine();
+            String[] pairArr = pair.split(" ");
+            int com1 = Integer.parseInt(pairArr[0]);
+            int com2 = Integer.parseInt(pairArr[1]);
+            if (!table[com1].contains(com2)) {
+                table[com1].add(com2);
             }
-            else {
-                List<Integer> list = new ArrayList<>();
-                list.add(b);
-                map.put(a,list);
-            }
-            if (map.keySet().contains(b)){
-                map.get(b).add(a);
-            }
-            else {
-                List<Integer> list = new ArrayList<>();
-                list.add(a);
-                map.put(b,list);
+            if (!table[com2].contains(com1)) {
+                table[com2].add(com1);
             }
         }
-        boolean[] vst = new boolean[Integer.parseInt(allComputerNum)+1];
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(1);
-        vst[1] = true;
+        boolean[] check = new boolean[comNum+1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        checkFuntion(1, deque, table, check);
+        while (!deque.isEmpty()) {
+            int num = deque.pollFirst();
+            checkFuntion(num, deque, table, check);
+        }
+        int answerCount = 0;
+        for (int i=0; i<comNum+1; i++) {
+            if (check[i]) {
+                answerCount++;
+            }
+        }
+        bw.write(String.valueOf(answerCount-1));
+        bw.close();
+    }
 
+    static void checkFuntion (int num, Deque deque, List[] table, boolean[] check) {
+        for (int i=0; i<table[num].size(); i++) {
+            int testNum = (int) table[num].get(i);
+            if (!check[testNum]){
+                check[testNum] = true;
+                deque.add(testNum);
+            }
+        }
     }
 }
