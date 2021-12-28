f1d00a017a866ca2e3fe114effd629e2a3baded3
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-12-28 17:09:24 +0900

    12.28 스택 수열 문제 미해결. 처음부터 알고리즘 생각을 잘못한거 같음. 내일 다시 문제 풀기

---
+++ src/silver3/S3_1874.java
@@ -0,0 +1,111 @@
+package silver3;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class S3_1874 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(bf.readLine());
+        int[] wantArr = new int[num];
+        boolean[] checkTable = new boolean[num];
+//        boolean check = true;
+//        for (int i=0; i<num; i++) {
+//            wantArr[i] = Integer.parseInt(bf.readLine());
+//            checkTable[wantArr[i]-1] = true;
+//            if (check) {
+//                for (int j = wantArr[i]; j < num - 1; j++) {
+//                    if (!checkTable[j] && checkTable[j + 1]) {
+//                        check = false;
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//        if (check) {
+//            Stack<Integer> stack = new Stack<>();
+//            int arrNum = 0;
+//            for (int i=1; i<=num; i++) {
+//                if (stack.empty()){
+//                    stack.push(i);
+//                    bw.write("+\n");
+//                }
+//                else if (stack.peek()!=wantArr[arrNum]) {
+//                    stack.push(i);
+//                    bw.write("+\n");
+//                }
+//                else if (stack.peek()==wantArr[arrNum]) {
+//                    while (stack.peek()==wantArr[arrNum]) {
+//                        stack.pop();
+//                        arrNum++;
+//                        bw.write("-\n");
+//                        if (stack.empty()) break;
+//                    }
+//                    stack.push(i);
+//                    bw.write("+\n");
+//                }
+//            }
+//            for (int i=0; i<stack.size(); i++) {
+//                bw.write("-\n");
+//            }
+//        }
+//        else {
+//            bw.write("NO");
+//        }
+
+        for (int i=0; i<num; i++) {
+            wantArr[i] = Integer.parseInt(bf.readLine());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int arrNum = 0;
+        List<String> answer = new ArrayList<>();
+        for (int i=1; i<=num; i++) {
+            if (stack.empty()){
+                stack.push(i);
+                answer.add("+");
+            }
+            else if (wantArr[arrNum]< stack.peek()) {
+                answer.clear();
+                answer.add("NO");
+                break;
+            }
+            else if (stack.peek()!=wantArr[arrNum]) {
+                stack.push(i);
+                answer.add("+");
+            }
+            else if (stack.peek()==wantArr[arrNum]) {
+                while (stack.peek()==wantArr[arrNum]) {
+                    stack.pop();
+                    arrNum++;
+                    answer.add("-");
+                    if (stack.empty()) {
+                        break;
+                    }
+                    else if (wantArr[arrNum]< stack.peek()) {
+                        answer.clear();
+                        answer.add("NO");
+                        break;
+                    }
+                }
+                stack.push(i);
+                answer.add("+");
+            }
+            if (answer.get(0).equals("NO")) {
+                break;
+            }
+        }
+        if (!answer.get(0).equals("NO")) {
+            for (int i = 0; i < stack.size(); i++) {
+                answer.add("-");
+            }
+        }
+        for (String s : answer) {
+            bw.write(s+"\n");
+        }
+        bw.close();
+    }
+}
