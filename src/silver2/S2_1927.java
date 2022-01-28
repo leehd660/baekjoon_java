883f5d8046f067d850cc391eb2a2fee65bc04c07
leehd660 <86194199+leehd660@users.noreply.github.com>
2022-01-28 20:57:04 +0900

    22.01.28 최소 힙 문제 해결. PriorityQueue를 이용하여 한방에 해결. priorityQueue는 사기다

--- src/silver2/S2_1927.java
+++ src/silver2/S2_1927.java
@@ -1,48 +1,72 @@
 package silver2;
 
 import java.io.*;
-import java.util.*;
+import java.util.PriorityQueue;
 
 public class S2_1927 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = Integer.parseInt(bf.readLine());
-//        Deque<Integer> deque = new ArrayDeque<>();
-//        Stack<Integer> stack = new Stack<>();
-        List<Integer> list = new ArrayList<>();
-        for (int i=0; i<num; i++) {
-            int n = Integer.parseInt(bf.readLine());
-            if (n == 0) {
-                if (list.isEmpty()) {
-                    bw.write(String.valueOf(0+"\n"));
+        int testNum = Integer.parseInt(bf.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i=0; i<testNum; i++) {
+            int num = Integer.parseInt(bf.readLine());
+            if (num == 0) {
+                if (queue.isEmpty()) {
+                    bw.write("0\n");
                 }
                 else {
-                    bw.write(String.valueOf(list.get(0)) + "\n");
-                    list.remove(0);
+                    bw.write(String.valueOf(queue.poll())+"\n");
                 }
             }
             else {
-                list.add(n);
-                Collections.sort(list);
-//                while (true) {
-//                    if (deque.isEmpty()) {
-//                        deque.addFirst(n);
-//                        break;
-//                    }
-//                    else if (deque.peekFirst() < n) {
-//                        stack.add(deque.pollFirst());
-//                    }
-//                    else {
-//                        deque.addFirst(n);
-//                        break;
-//                    }
-//                }
-//                while (!stack.isEmpty()) {
-//                    deque.addFirst(stack.pop());
-//                }
+                queue.add(num);
             }
         }
         bw.close();
     }
 }
+
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        int num = Integer.parseInt(bf.readLine());
+////        Deque<Integer> deque = new ArrayDeque<>();
+////        Stack<Integer> stack = new Stack<>();
+//        List<Integer> list = new ArrayList<>();
+//        for (int i=0; i<num; i++) {
+//            int n = Integer.parseInt(bf.readLine());
+//            if (n == 0) {
+//                if (list.isEmpty()) {
+//                    bw.write(String.valueOf(0+"\n"));
+//                }
+//                else {
+//                    bw.write(String.valueOf(list.get(0)) + "\n");
+//                    list.remove(0);
+//                }
+//            }
+//            else {
+//                list.add(n);
+//                Collections.sort(list);
+////                while (true) {
+////                    if (deque.isEmpty()) {
+////                        deque.addFirst(n);
+////                        break;
+////                    }
+////                    else if (deque.peekFirst() < n) {
+////                        stack.add(deque.pollFirst());
+////                    }
+////                    else {
+////                        deque.addFirst(n);
+////                        break;
+////                    }
+////                }
+////                while (!stack.isEmpty()) {
+////                    deque.addFirst(stack.pop());
+////                }
+//            }
+//        }
+//        bw.close();
+//    }
+
