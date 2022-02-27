ce9a7c71a9da30233254243fe73868668e417705
leehd660 <86194199+leehd660@users.noreply.github.com>
2022-02-27 16:05:13 +0900

    22.02.27 벽 부수고 이동하기 문제 미해결. 벽을 부수면서 이동하기 위해서 DFS방법을 이용했지만, 역시나 시간초과. 방법을 찾아보니 visited를 3중배열로 만들어서 해결해야함. 벽을 부신적이 있는지 없는지. DP방법이 생각 나는데 음 그건 안될거 같은데 이상하다.. 다음에 다시 풀어보기,,

--- src/gold4/G4_2206.java
+++ src/gold4/G4_2206.java
@@ -1,22 +1,19 @@
 package gold4;
 
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class G4_2206 {
     private static int N;
     private static int M;
-    private static int count;
+    private static int crashCount;
     private static int answer;
     private static int[] xArr;
     private static int[] yArr;
     private static int[][] map;
     private static boolean[][] visited;
-    private static Deque<Integer> dequeX;
-    private static Deque<Integer> dequeY;
+    private static List<Integer> listX;
+    private static List<Integer> listY;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -25,7 +22,6 @@ public class G4_2206 {
         N = Integer.parseInt(NMtoken.nextToken());
         M = Integer.parseInt(NMtoken.nextToken());
         map = new int[N][M];
-        visited = new boolean[N][M];
         for (int i=0; i<N; i++) {
             String str = bf.readLine();
             String[] strArr = str.split("");
@@ -33,23 +29,17 @@ public class G4_2206 {
                 map[i][j] = Integer.parseInt(strArr[j]);
             }
         }
+        crashCount = 0;
+        answer = N*M;
         xArr = new int[]{0,0,1,-1};
         yArr = new int[]{1,-1,0,0};
-        dequeX = new ArrayDeque<>();
-        dequeY = new ArrayDeque<>();
-        answer = N*M;
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<M; j++) {
-                if (map[i][j] == 1) {
-                    map[i][j] = 0;
-                    checkRoad();
-                    map[i][j] = 1;
-                    for (int k=0; k<N; k++) {
-                        Arrays.fill(visited[k],false);
-                    }
-                }
-            }
-        }
+        visited = new boolean[N][M];
+        listX = new ArrayList<>();
+        listY = new ArrayList<>();
+        listX.add(0);
+        listY.add(0);
+        visited[0][0] = true;
+        DFS();
         if (answer == N*M) {
             bw.write("-1");
         }
@@ -59,32 +49,122 @@ public class G4_2206 {
         bw.close();
     }
 
-    static void checkRoad() {
-        dequeX.add(0);
-        dequeY.add(0);
-        count = 1;
-        visited[0][0] = true;
-        while (!dequeX.isEmpty() && !visited[N-1][M-1]) {
-            int size = dequeX.size();
-            for (int s=0; s<size; s++) {
-                int x = dequeX.pollFirst();
-                int y = dequeY.pollFirst();
-                for (int i = 0; i < 4; i++) {
-                    if (x + xArr[i] >= 0 && x + xArr[i] < N && y + yArr[i] >= 0 && y + yArr[i] < M && map[x + xArr[i]][y + yArr[i]] == 0 && !visited[x + xArr[i]][y + yArr[i]]) {
+    static void DFS () {
+        if (visited[N-1][M-1]) {
+            answer = Math.min(answer, listX.size());
+        }
+        else {
+            int x = listX.get(listX.size() - 1);
+            int y = listY.get(listY.size() - 1);
+            for (int i = 0; i < 4; i++) {
+                if (x + xArr[i] >= 0 && x + xArr[i] < N && y + yArr[i] >= 0 && y + yArr[i] < M && !visited[x + xArr[i]][y + yArr[i]]) {
+                    if (crashCount == 0 && map[x + xArr[i]][y + yArr[i]] == 1) {
+                        //벽을 만나면, DFS->BFS 변경 가능하게 만들어야하나?
+                        crashCount = 1;
+                        listX.add(x + xArr[i]);
+                        listY.add(y + yArr[i]);
                         visited[x + xArr[i]][y + yArr[i]] = true;
-                        dequeX.add(x+xArr[i]);
-                        dequeY.add(y+yArr[i]);
+                        DFS();
+                        crashCount = 0;
+                        listX.remove(listX.size()-1);
+                        listY.remove(listX.size()-1);
+                        visited[x + xArr[i]][y + yArr[i]] = false;
+                    } else if (map[x + xArr[i]][y + yArr[i]] == 0) {
+                        listX.add(x + xArr[i]);
+                        listY.add(y + yArr[i]);
+                        visited[x + xArr[i]][y + yArr[i]] = true;
+                        DFS();
+                        listX.remove(listX.size()-1);
+                        listY.remove(listX.size()-1);
+                        visited[x + xArr[i]][y + yArr[i]] = false;
                     }
                 }
             }
-            count++;
-//            if (visited[N-1][M-1]) break;
         }
-        dequeX.clear();
-        dequeY.clear();
-        if (visited[N-1][M-1]) {
-            answer = Math.min(count, answer);
-        }
-        count = 1;
     }
 }
+
+
+//    모든 벽을 탐색해서 벽을 하나씩 제거한 후 BFS방식을 취한 방법. -> 이 방법 시, 시간 초과 발생
+
+//public class G4_2206 {
+//    private static int N;
+//    private static int M;
+//    private static int count;
+//    private static int answer;
+//    private static int[] xArr;
+//    private static int[] yArr;
+//    private static int[][] map;
+//    private static boolean[][] visited;
+//    private static Deque<Integer> dequeX;
+//    private static Deque<Integer> dequeY;
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer NMtoken = new StringTokenizer(bf.readLine());
+//        N = Integer.parseInt(NMtoken.nextToken());
+//        M = Integer.parseInt(NMtoken.nextToken());
+//        map = new int[N][M];
+//        visited = new boolean[N][M];
+//        for (int i=0; i<N; i++) {
+//            String str = bf.readLine();
+//            String[] strArr = str.split("");
+//            for (int j=0; j<M; j++) {
+//                map[i][j] = Integer.parseInt(strArr[j]);
+//            }
+//        }
+//        xArr = new int[]{0,0,1,-1};
+//        yArr = new int[]{1,-1,0,0};
+//        dequeX = new ArrayDeque<>();
+//        dequeY = new ArrayDeque<>();
+//        answer = N*M;
+//        for (int i=0; i<N; i++) {
+//            for (int j=0; j<M; j++) {
+//                if (map[i][j] == 1) {
+//                    map[i][j] = 0;
+//                    checkRoad();
+//                    map[i][j] = 1;
+//                    for (int k=0; k<N; k++) {
+//                        Arrays.fill(visited[k],false);
+//                    }
+//                }
+//            }
+//        }
+//        if (answer == N*M) {
+//            bw.write("-1");
+//        }
+//        else {
+//            bw.write(String.valueOf(answer));
+//        }
+//        bw.close();
+//    }
+//
+//    static void checkRoad() {
+//        dequeX.add(0);
+//        dequeY.add(0);
+//        count = 1;
+//        visited[0][0] = true;
+//        while (!dequeX.isEmpty() && !visited[N-1][M-1]) {
+//            int size = dequeX.size();
+//            for (int s=0; s<size; s++) {
+//                int x = dequeX.pollFirst();
+//                int y = dequeY.pollFirst();
+//                for (int i = 0; i < 4; i++) {
+//                    if (x + xArr[i] >= 0 && x + xArr[i] < N && y + yArr[i] >= 0 && y + yArr[i] < M && map[x + xArr[i]][y + yArr[i]] == 0 && !visited[x + xArr[i]][y + yArr[i]]) {
+//                        visited[x + xArr[i]][y + yArr[i]] = true;
+//                        dequeX.add(x+xArr[i]);
+//                        dequeY.add(y+yArr[i]);
+//                    }
+//                }
+//            }
+//            count++;
+//        }
+//        dequeX.clear();
+//        dequeY.clear();
+//        if (visited[N-1][M-1]) {
+//            answer = Math.min(count, answer);
+//        }
+//        count = 1;
+//    }
+//}
