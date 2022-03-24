88fc291f26609febd6fdf59fa5b77d012469bb54
leehd660 <86194199+leehd660@users.noreply.github.com>
2022-03-24 20:35:02 +0900

    no message

--- src/SWM2022/codingTest1/num2.java
+++
@@ -1,8 +0,0 @@
-package SWM2022.codingTest1;
-
-public class num2 {
-    //산사태자료 구조라고, 레벨 층이 있고, 층마다 크기가 있음
-    //레벨 1에 key:value형태로 넣음 -> 크기가 넘치면 전부 아래층으로 내려감
-    //insert함수랑 find함수 만드는 거였는데, key가 같은 값이 있으면, 메모리를 합치랬나,,,,
-    //find함수는 key값으로 메모리 크기 찾는거
-}
